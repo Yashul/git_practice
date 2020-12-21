54f925305f831f8c47f49646231569c2eb0db72a
Yashul <yashulaggarwal@gmail.com>
2020-12-21 19:33:00 +0530

    Login Page first commit

---
+++ login.js
@@ -0,0 +1,7 @@
+var attempt = 3; //Variable to count number of attempts
+
+function validate(){
+    var username = document.getElementById("username").nodeValue;
+    var password = document.getElementById("password").nodeValue;
+    if(username)
+}
